919b3557f7e126921b6c6f51c8643af0dc6106b6
lokosuns <lokosuns@gmail.com>
2021-09-28 15:16:57 +0300

    Initialization project

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import main_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_view),
+
 ]
